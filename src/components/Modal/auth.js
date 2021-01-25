40db96f6add5123a2ccdf2de2759232a5a6ccd2b
Andrey <adkurgin@sberbank.ru>
2021-01-25 18:45:09 +0300

    confirm password when register

--- src/components/Modal/auth.js
+++ src/components/Modal/auth.js
@@ -9,13 +9,17 @@ import styles from './style.js';
 
 const ModalAuth = (props) => {
   const [userName, setUserName] = useState();
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [error, setError] = useState(null);
 
   const dispatch = useDispatch();
   const setUserData = (user) => dispatch(setUser(user));
 
   const handleLogin = () => {
+    setShowConfirmPassword(false);
+
     auth()
       .signInWithEmailAndPassword(userName, password)
       .then((res) => {
@@ -29,17 +33,25 @@ const ModalAuth = (props) => {
   };
 
   const handleRegister = () => {
-    auth()
-      .createUserWithEmailAndPassword(userName, password)
-      .then((res) => {
-        setError(null);
-        setUserData(res.user);
-        props.setShowModal(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-    // props.navigation.navigate('ProfileScreen');
+    if (password.length > 0 && password === confirmPassword) {
+      auth()
+        .createUserWithEmailAndPassword(userName, password)
+        .then((res) => {
+          setError(null);
+          setUserData(res.user);
+          props.setShowModal(false);
+          props.navigation.navigate('ProfileScreen');
+        })
+        .catch((err) => {
+          setError(err.message);
+        });
+    } else {
+      if (!showConfirmPassword) {
+        setShowConfirmPassword(true);
+      } else {
+        setError('Confirm Password');
+      }
+    }
   };
 
   return (
@@ -68,6 +80,17 @@ const ModalAuth = (props) => {
               placeholder="Password"
             />
           </Item>
+          {showConfirmPassword && (
+            <Item style={styles.input}>
+              <Input
+                textContentType="password"
+                secureTextEntry
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                placeholder="Confirm Password"
+              />
+            </Item>
+          )}
           {error && <Text style={styles.error}>{error}</Text>}
           <Button
             full
