d46a903125283b5336aea0ca4cf3ffdbc383e4d2
Andrew <trumanpvt@gmail.com>
2021-02-13 16:35:38 +0300

    eslint crlf lf fix

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -3,7 +3,7 @@ import {Container, Content} from 'native-base';
 import Articles from '../Articles';
 import Podcasts from '../Podcasts';
 import Faq from '../Faq';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import {StyleSheet} from 'react-native';
 import AppFooter from '../AppFooter';
 import {observer} from 'mobx-react-lite';
@@ -41,5 +41,5 @@ const HomeScreen = observer(() => {
   );
 });
 
-HomeScreen.propTypes = {};
+// HomeScreen.propTypes = {};
 export default HomeScreen;
