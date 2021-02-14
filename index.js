ed364a862b06491de33995ac681ecad277530251
Andrew <trumanpvt@gmail.com>
2021-02-14 19:39:46 +0300

    amplify configure

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 import App from './src/App';
 import {name as appName} from './src/app.json';
 
