ba1f29435ef8c24962fe364f08f9758ac132f2c6
Andrey <adkurgin@sberbank.ru>
2021-04-06 17:44:29 +0300

    initial commit

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -4,19 +4,21 @@ import {Body, Button, Header, Icon, Left, Right, Title} from 'native-base';
 
 import styles from './style.js';
 
-const AppHeader = (props) => {
+export interface Props {
+  scene: object;
+}
+
+const AppHeader: React.FC<Props> = ({scene}) => {
   return (
     <View style={styles.header}>
       <Header>
         <Left>
-          <Button
-            transparent
-            onPress={props.scene.descriptor.navigation.openDrawer}>
+          <Button transparent onPress={scene.descriptor.navigation.openDrawer}>
             <Icon type="MaterialIcons" name="menu" />
           </Button>
         </Left>
         <Body>
-          <Title>{props.scene.route.name}</Title>
+          <Title>{scene.route.name}</Title>
         </Body>
         <Right>
           {/*<Button transparent>*/}
